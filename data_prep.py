8daaf2b53e532f198dce0158470f70823f6e2b73
Deni Priyadi <56055145+DeniPriyadi18@users.noreply.github.com>
2025-05-24 21:00:02 +0700

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -6,7 +6,7 @@ sys.modules['__main__'].convert_to_df = custom_transformers.convert_to_df
 sys.modules['__main__'].ColumnSelectorTransformer = custom_transformers.ColumnSelectorTransformer
 sys.modules['__main__'].ToDataFrameTransformer = custom_transformers.ToDataFrameTransformer
 
-prepro= joblib.load('Model/preprocessing_pipe.joblib')
+prepro= joblib.load('Model/Encoder/preprocessing_pipe.joblib')
 final_columns= joblib.load('Model/final_columns.joblib')
 
 def preprocessing(df):
